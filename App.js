11467f3676f96b7b065938d5e9bd192b409db297
DrumKit <sumitbhargav12@gmail.com>
2024-04-24 11:18:26 +0530

    feact data

--- App.js
+++ App.js
@@ -4,9 +4,15 @@ import { Text, View, StyleSheet } from 'react-native';
 // import TextInputComponent from './src/components/reactNativeBasics/DailyClass/TextInpuComponent';
 // import SearchUseEffectComponent from './src/components/reactNativeBasics/DailyClass/SearchUseEffectComponents';
 // import ScrollviewComponent from './src/components/reactNativeBasics/DailyClass/ScrollviewComponent';
+// import New from './src/components/reactNativeBasics/DailyClass/New ';
 // import KeyboardAvoidingViewComponent from './src/components/reactNativeBasics/DailyClass/KeyBoardAvoidComponents';
 // import ImageComponent from './src/components/reactNativeBasics/DailyClass/ImageComponents';
-import FlatListUseEffectAssignment from './src/components/reactNativeBasics/DailyClass/FlatListUseEffectAssignment';
+// import FlatListUseEffectAssignment from './src/components/reactNativeBasics/DailyClass/FlatListUseEffectAssignment';
+// import ButtonComponent from './src/components/reactNativeBasics/DailyClass/ButtonComponents';
+// import UseEffectComponents from './src/components/reactNativeBasics/DailyClass/UseEffectComponents';
+import FetchDataComponent from './src/components/reactNativeBasics/DailyClass/Fetch';
+// import Arrayof1o from './src/components/reactNativeBasics/Assignment/Arrayof10'
+
 const App = () => {
   const styles = StyleSheet.create({
    
@@ -17,15 +23,19 @@ const App = () => {
    <Text style={{fontSize:30}}></Text>
 
    {/* Daily Class*/ }
-
-{/* <ButtonComponent/> */}
+   {/* <UseEffectComponents/> */}
 {/* <TextInputComponent/> */}
 {/* <SearchUseEffectComponent/> */}
 {/* <ScrollviewComponent/> */}
+{/* <New/> */}
 {/* <KeyboardAvoidingViewComponent/> */}
 {/* <ImageComponent/> */}
-<FlatListUseEffectAssignment/>
-   
+{/* <FlatListUseEffectAssignment/> */}
+ {/* <ButtonComponent/>   */}
+<FetchDataComponent/>
+
+ {/* Assignment*/}
+{/* <Arrayof1o/> */}
 
     </View>
   )
---
+++ src/components/reactNativeBasics/DailyClass/Fetch.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import axios from 'axios'
+const FetchDataComponent = () => {
+    const [data, setData] = useState([]);
+    // const fetchData = async () =>{
+    //    const response =await fetch(`https://fakestoreapi.com/products`);
+    //    console.log(response)
+    //    const jsonData =await response.json();
+    //    console.log(jsonData) 
+    // }
+    const fetchData = async () =>{
+        const response = await axios.get(`https://fakestoreapi.com/products`)
+        // console.log(response.data)
+        // const datas = response.data
+        setData(response.data);
+    }
+    
+    fetchData();
+  return (
+    <View>
+      <Text>FetchData</Text>
+      {data.map((product)=>(
+        <Text>Title: {product.title.substring(0,10)}</Text>
+      ))}
+    </View>
+  )
+}
+
+export default FetchDataComponent
+
+const styles = StyleSheet.create({})
